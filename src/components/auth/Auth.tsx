b89241983023cf8207f9dcdce88974f6520fb05d
Obemych <beksultanovich.911@gmail.com>
2024-05-17 17:20:48 +0600

    adaptive auth

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -25,7 +25,15 @@ const Auth: FC<IAuthProps> = ({ isOpen, close, type = "register" }) => {
       isOpen={isOpen}
       close={close}
       modalStyle="z-[60]"
-      contentStyle="pt-20 px-40 pb-[32px] max-w-[600px] w-full font-mulish"
+      contentStyle="  pt-20 px-40 pb-[32px] w-[600px] w-full
+       blt:pt-[16px] blt:pr-[36px] blt:pb-[28px] blt:pl-[36px] blt:w-[600px]
+lt:pt-[14px] lt:pr-[28px] lt:pb-[24px] lt:pl-[28px] lt:w-[500px]
+tb:pt-[12px] tb:pr-[24px] tb:pb-[20px] tb:pl-[24px] tb:w-[400px]
+stb:pt-[10px] stb:pr-[20px] stb:pb-[16px] stb:pl-[20px] stb:w-[300px]
+bmb:pt-[8px] bmb:pr-[16px] bmb:pb-[12px] bmb:pl-[16px] bmb:w-[250px]
+amb:pt-[6px] amb:pr-[12px] amb:pb-[8px] amb:pl-[12px] amb:w-[200px]
+smb:pt-[5px] smb:pr-[10px] smb:pb-[6px] smb:pl-[10px] smb:w-[180px] ont-mulish"
+      
     >
       {authType === "login" || authType === "register" ? (
         <RegisterOrLogin authType={authType} setAuthType={setAuthType} />
--- src/components/auth/registerOrLogin/RegisterOrLogin.tsx
+++ src/components/auth/registerOrLogin/RegisterOrLogin.tsx
@@ -43,12 +43,38 @@ const RegisterOrLogin: FC<Props> = ({ authType, setAuthType }) => {
 
   return (
     <>
-      <h2 className="font-montserrat">
-        {authType === "login" ? "Войти" : "Зарегистрироваться"}
-      </h2>
+    <h2 className="
+  font-montserrat 
+  text-4xl 
+  blt:text-3xl 
+  lt:text-2xl 
+  tb:text-xl 
+  stb:text-lg 
+  bmb:text-base 
+  amb:text-sm 
+  smb:text-xs
+">
+  {authType === "login" ? "Войти" : "Зарегистрироваться"}
+</h2>
+
       <form onSubmit={(e) => e.preventDefault()}>
-        <div className="mt-[56px] mb-[32px]">
-          <span className="text-14 text-[rgba(83,83,84,1)]">
+        <div className="  mt-[56px] 
+  mb-[32px] 
+  blt:mt-[48px] blt:mb-[24px] 
+  lt:mt-[40px] lt:mb-[20px] 
+  tb:mt-[32px] tb:mb-[16px] 
+  stb:mt-[24px] stb:mb-[12px] 
+  bmb:mt-[16px] bmb:mb-[8px] 
+  amb:mt-[12px] amb:mb-[6px] 
+  smb:mt-[8px] smb:mb-[4px]">
+          <span className="  text-14 text-[rgba(83,83,84,1)] 
+    blt:text-base blt:text-[rgba(83,83,84,1)] 
+    lt:text-sm lt:text-[rgba(83,83,84,1)] 
+    tb:text-xs tb:text-[rgba(83,83,84,1)] 
+    stb:text-xs stb:text-[rgba(83,83,84,1)] 
+    bmb:text-xs bmb:text-[rgba(83,83,84,1)] 
+    amb:text-xs amb:text-[rgba(83,83,84,1)] 
+    smb:text-xs smb:text-[rgba(83,83,84,1)]">
             Введите Email<span className="text-red">*</span>
           </span>
           <Input
@@ -60,12 +86,19 @@ const RegisterOrLogin: FC<Props> = ({ authType, setAuthType }) => {
                 value:
                   /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu,
                 message: "Некорректный Email!",
-              },
+              },  
             })}
           />
         </div>
         <div>
-          <span className="text-14 text-[rgba(83,83,84,1)]">
+          <span className="  text-14 text-[rgba(83,83,84,1)] 
+    blt:text-base blt:text-[rgba(83,83,84,1)] 
+    lt:text-sm lt:text-[rgba(83,83,84,1)] 
+    tb:text-xs tb:text-[rgba(83,83,84,1)] 
+    stb:text-xs stb:text-[rgba(83,83,84,1)] 
+    bmb:text-xs bmb:text-[rgba(83,83,84,1)] 
+    amb:text-xs amb:text-[rgba(83,83,84,1)] 
+    smb:text-xs smb:text-[rgba(83,83,84,1)]">
             Введите пароль<span className="text-red">*</span>
           </span>
           <div className="relative flex items-center">
@@ -103,7 +136,21 @@ const RegisterOrLogin: FC<Props> = ({ authType, setAuthType }) => {
               {errors.password.message}
             </span>
           )}
-          <span className="mt-[12px] mb-[32px] block text-end text-14">
+          <span className=" mt-[12px] mb-[32px] 
+  blt:mt-[16px] blt:mb-[24px] 
+  lt:mt-[14px] lt:mb-[28px] 
+  tb:mt-[12px] tb:mb-[24px] 
+  stb:mt-[10px] stb:mb-[20px] 
+  bmb:mt-[8px] bmb:mb-[16px] 
+  amb:mt-[6px] amb:mb-[12px] 
+  smb:mt-[4px] smb:mb-[8px] block text-end   text-14 text-[rgba(83,83,84,1)] 
+    blt:text-base blt:text-[rgba(83,83,84,1)] 
+    lt:text-sm lt:text-[rgba(83,83,84,1)] 
+    tb:text-xs tb:text-[rgba(83,83,84,1)] 
+    stb:text-xs stb:text-[rgba(83,83,84,1)] 
+    bmb:text-xs bmb:text-[rgba(83,83,84,1)] 
+    amb:text-xs amb:text-[rgba(83,83,84,1)] 
+    smb:text-xs smb:text-[rgba(83,83,84,1)]">
             <span className="text-red">*</span> - обязательное поле
           </span>
           <div className="flex justify-between">
@@ -124,26 +171,50 @@ const RegisterOrLogin: FC<Props> = ({ authType, setAuthType }) => {
                   className="opacity-0 group-checked:opacity-100"
                 />
               </div>
-              <span className="text-14 text-[rgba(108,108,108,1)] leading-[19px]">
+              <span className="  text-14 text-[rgba(83,83,84,1)] 
+    blt:text-base blt:text-[rgba(83,83,84,1)] 
+    lt:text-sm lt:text-[rgba(83,83,84,1)] 
+    tb:text-xs tb:text-[rgba(83,83,84,1)] 
+    stb:text-xs stb:text-[rgba(83,83,84,1)] 
+    bmb:text-xs bmb:text-[rgba(83,83,84,1)] 
+    amb:text-xs amb:text-[rgba(83,83,84,1)] 
+    smb:text-xs smb:text-[rgba(83,83,84,1)] leading-[19px]">
                 Запомнить меня
               </span>
             </label>
             {authType === "login" && (
               <button
                 onClick={() => setAuthType("forgot-password")}
-                className="text-blue text-14 leading-[19px]"
+                className="text-blue  text-14  
+    blt:text-base blt:text-blue
+    lt:text-sm lt:text-blue
+    tb:text-xs tb:text-blue
+    stb:text-xs stb:text-blue
+    bmb:text-xs bmb:text-blue 
+    amb:text-xs amb:text-blue
+    smb:text-xs smb:text-blue leading-[19px]"
               >
                 Забыли пароль?
               </button>
             )}
           </div>
-          <button
-            onClick={handleSubmit(authFunc)}
-            disabled={isPending || !isValid}
-            className="btn my-[32px] py-[22px] w-full"
-          >
-            {authType === "login" ? "Войти" : "Создать аккаунт"}
-          </button>
+         <button
+  onClick={handleSubmit(authFunc)}
+  disabled={isPending || !isValid}
+  className="
+    btn my-[32px] py-[22px] w-full 
+    blt:py-[20px] blt:my-[30px] 
+    lt:py-[18px] lt:my-[28px] 
+    tb:py-[16px] tb:my-[26px] 
+    stb:py-[14px] stb:my-[24px] 
+    bmb:py-[12px] bmb:my-[22px] 
+    amb:py-[10px] amb:my-[20px] 
+    smb:py-[8px] smb:my-[18px]
+  "
+>
+  {authType === "login" ? "Войти" : "Создать аккаунт"}
+</button>
+
           <span className="block text-center text-14 leading-[19px] text-[rgba(51,51,51,1)]">
             {authType === "login" ? "Ещё нет аккаунта?" : "Есть аккаунт?"}{" "}
             <button
@@ -154,12 +225,35 @@ const RegisterOrLogin: FC<Props> = ({ authType, setAuthType }) => {
               {authType === "login" ? "Зарегистрируйтесь" : "Войдите"}
             </button>
           </span>
-          <div className="mt-[32px] flex justify-between">
-            <button className="rounded-[24px] border border-[rgba(0,0,0,0.12)] py-[17px] pl-[38px] pr-[25px] flex gap-[9px] items-center text-[rgba(51,51,51,1)]">
-              <img src={googleIcon} alt="google" />
-              <span>Войти через Google</span>
-            </button>
-            <button className="rounded-[24px] border border-[rgba(0,0,0,0.12)] py-[15px] pl-[26px] pr-[13px] flex gap-[9px] items-center text-[rgba(51,51,51,1)]">
+          <div className="mt-[32px] flex justify-between items-center gap-2">
+    <button className="
+    text-[16px] blt:text-[14px] lt:text-[12px] tb:text-[10px] stb:text-[8px] bmb:text-[6px] amb:text-[5px] smb:text-[4px]
+  rounded-[24px] border border-[rgba(0,0,0,0.12)] 
+  py-[17px] pl-[38px] pr-[25px] 
+  flex gap-[9px] items-center 
+  text-[rgba(51,51,51,1)] 
+  blt:py-[16px] blt:pl-[36px] blt:pr-[24px] 
+  lt:py-[13px] lt:pl-[30px] lt:pr-[18px] 
+  tb:py-[10px] tb:pl-[24px] tb:pr-[12px] 
+  stb:py-[7px] stb:pl-[18px] stb:pr-[8px] 
+  bmb:py-[6px] bmb:pl-[12px] bmb:pr-[6px] 
+  amb:py-[5px] amb:pl-[8px] amb:pr-[4px] 
+  smb:py-[4px] smb:pl-[4px] smb:pr-[2px]
+">
+  <img src={googleIcon} alt="google" />
+  <span>Войти через Google</span>
+</button>
+
+            <button className=" text-[16px] blt:text-[14px] lt:text-[12px] tb:text-[10px] stb:text-[8px] bmb:text-[6px] amb:text-[5px] smb:text-[4px] rounded-[24px] border border-[rgba(0,0,0,0.12)] py-[17px] pl-[38px] pr-[25px] flex gap-[9px] items-center text-[rgba(51,51,51,1)]  
+
+  blt:py-[16px] blt:pl-[36px] blt:pr-[24px] 
+  lt:py-[13px] lt:pl-[30px] lt:pr-[18px] 
+  tb:py-[10px] tb:pl-[24px] tb:pr-[12px] 
+  stb:py-[7px] stb:pl-[18px] stb:pr-[8px] 
+  bmb:py-[6px] bmb:pl-[12px] bmb:pr-[6px] 
+  amb:py-[5px] amb:pl-[8px] amb:pr-[4px] 
+  smb:py-[4px] smb:pl-[4px] smb:pr-[2px]
+">
               <div className="rounded-[30px] w-[30px] h-[30px] flex justify-center items-center bg-[linear-gradient(0deg,#0062E0_2.92%,#19AFFF_100%)]">
                 <img src={facebookIcon} alt="facebook" />
               </div>
